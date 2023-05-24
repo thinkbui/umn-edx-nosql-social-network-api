c7b9f9ec3b48ec06ea04268747c4676dcec5ad05
Ian Bui <thinkbui@gmail.com>
2023-05-23 20:32:29 -0700

    Updated connection to use Mongo

--- config/connection.js
+++ config/connection.js
@@ -1,30 +1,11 @@
-/*
-  This version of the connection file will work locally,
-  as well as when you deploy a MySQL app to Heroku.
+const { connect, connection } = require('mongoose');
 
-  JAWS_DB is an add-on offered by Heroku that provides 
-  you with a hosted MySQL database.
+const connectionString =
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialNetworkDB';
 
-*/
+connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-const Sequelize = require('sequelize');
-require('dotenv').config();
-
-let sequelize;
-
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    }
-  );
-}
-
-module.exports = sequelize;
+module.exports = connection;
