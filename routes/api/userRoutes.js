debe1b7eda318c0a73b744308de7a8c2da30b9b2
Ian Bui <thinkbui@gmail.com>
2023-05-24 23:15:32 -0700

    Added removeFriend

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -6,12 +6,13 @@ const {
   updateUser,
   deleteUser,
   addFriend,
+  removeFriend,
 } = require('../../controllers/userController');
 
 router.route('/').get(getUsers).post(createUser);
 
 router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
 
-router.route('/:userId/friends/:friendId').post(addFriend);
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
 
 module.exports = router;
