75954700096f9f1aa8f97e79e2693e5ef6942295
Ian Bui <thinkbui@gmail.com>
2023-05-24 17:34:05 -0700

    Added populated friends and thoughts to getSingleUser

--- models/User.js
+++ models/User.js
@@ -15,13 +15,13 @@ const userSchema = new Schema(
     thoughts: [
       {
         type: Schema.Types.ObjectId,
-        ref: 'Thought',
+        ref: 'thought',
       },
     ],
     friends: [
       {
         type: Schema.Types.ObjectId,
-        ref: 'User',
+        ref: 'user',
       },
     ],
   },
