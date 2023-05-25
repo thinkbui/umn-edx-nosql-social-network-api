beecc78b89b1fcaf4163cb96c630e058b5d2c9cf
Ian Bui <thinkbui@gmail.com>
2023-05-24 21:28:08 -0700

    Added updateUser

--- controllers/userController.js
+++ controllers/userController.js
@@ -37,6 +37,18 @@ module.exports = {
       .then((user) => res.json(user))
       .catch((err) => res.status(500).json(err));
   },
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No users found with those IDs :(' })
+          : res.json(user)
+      )
+  },
   deleteUser(req, res) {
     User.findOneAndRemove({ _id: req.params.userId })
       .then((user) => {
